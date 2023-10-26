124db64c63ba47d6b44a63a7ce3a4158f8a71698
yusukenemoto12345 <145512014+yusukenemoto12345@users.noreply.github.com>
2023-10-26 11:03:06 +0900

    ユーザー管理機能実装２

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      # nicknameが空では登録できないテストコードを記述します
+    end
+    it 'emailが空では登録できない' do
+      # emailが空では登録できないテストコードを記述します
+    end
+  end
+end
