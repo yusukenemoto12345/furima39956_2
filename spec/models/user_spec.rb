9bf3ee2042dc64b00d46542b9ec7a072db3f88e6
yusukenemoto12345 <145512014+yusukenemoto12345@users.noreply.github.com>
2023-10-26 18:47:40 +0900

    ユーザー管理機能5

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,31 +2,20 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context '異常系' do
-    # (略)
-    it '重複したメールアドレスは登録できないこと' do
-      User.create(username: 'testuser1', email: 'test@example.com', password: 'password')
-      user = User.new(username: 'testuser2', email: 'test@example.com', password: 'password')
-      expect(user).not_to be_valid
-    end
-
-    it 'メールアドレスに@を含まない場合は登録できないこと' do
-      user = User.new(username: 'testuser', email: 'invalid_email', password: 'password')
-      expect(user).not_to be_valid
-    end
-
-    it 'パスワードが空では登録できないこと' do
-      user = User.new(username: 'testuser', email: 'test@example.com', password: '')
-      expect(user).not_to be_valid
-    end
-
-    # 他のテストも同様に続きます...
-
     it '生年月日が空だと登録できないこと' do
-      user = User.new(username: 'testuser', email: 'test@example.com', password: 'password', birth_date: nil)
+      user = User.new(first_name_katakana: 'タロウ', last_name_katakana: 'タナカ', email: 'test@example.com', password: 'password')
       expect(user).not_to be_valid
     end
   end
 end
+
+
+
+
+
+
+
+
 
 
 
