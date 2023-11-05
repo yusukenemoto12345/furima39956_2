2e17de4fdc4649f6104777d2f57be99d3a0da1bf
yusukenemoto12345 <145512014+yusukenemoto12345@users.noreply.github.com>
2023-11-05 20:17:12 +0900

    商品一覧表示機能4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.order(created_at: :desc) # 新しい順に並べ替える
+    @shipping_fees = ShippingFee.all
   end
 
   def new
@@ -23,21 +24,22 @@ def create
   end
 end
 
-def show
-  @item = Item.find(params[:id])
-end
-
-def shipping_fee
-  # shipping_fee メソッドの定義
-  # 返す値を適切に設定する
-end
-
-def sold_out?
-  !sold_out？ # これは sold_out フラグが nil でないかどうかを確認するものです
-end
-
+#def show
+  #@item = Item.find(params[:id])
+#end
 
 private
+
+
+#def shipping_fee
+  # shipping_fee メソッドの定義
+  # 返す値を適切に設定する
+#end
+
+#def sold_out?
+  #!sold_out？ # これは sold_out フラグが nil でないかどうかを確認するものです
+#end
+
 
 def item_params
   params.require(:item).permit(:name, :item_image, :description, :price, :condition_id, :category_id, :shipping_fee_id, :prefecture_id, :shipping_date_id)
