4803f5df86bb12a44c210b19e5840ef45cb3f22f
yusukenemoto12345 <145512014+yusukenemoto12345@users.noreply.github.com>
2023-11-06 21:37:17 +0900

    商品削除機能１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
   before_action :move_to_index, only: [:edit, :update]
-  before_action :set_item, only: [:edit, :show, :update ]
+  before_action :set_item, only: [:edit, :show, :update ,:destroy]
 
 
 
@@ -43,6 +43,14 @@ end
 def edit
   
 end
+
+def destroy
+    @item.destroy
+    redirect_to root_path
+end
+
+
+
   #if current_user == @item.user && !Order.exists?(item_id: @item.id)
   #render :edit
   #else
